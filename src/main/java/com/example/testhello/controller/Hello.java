2778fda30c0a87129eb604a1d9c4b6584835f1fa
Lfw4 <1361702304@qq.com>
2021-04-23 22:50:35 +0800

    test

---
+++ src/main/java/com/example/testhello/controller/Hello.java
@@ -0,0 +1,16 @@
+package com.example.testhello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author刘锋伟
+ * @date 2021/4/23 21:21
+ */
+@RestController
+public class Hello {
+    @RequestMapping("hello")
+    public String getHello(){
+        return "hello world";
+    }
+}
